a8f2ca2aa1c71bb2ace61dc8b0cb9405324bb2b7
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-03 19:36:20 +0530

    Update count_digits.cpp

--- Basic Maths/count_digits.cpp
+++ Basic Maths/count_digits.cpp
@@ -5,17 +5,18 @@ int count(int n){
     int cnt = 0;
     int ogNo = n;
     while(n>0){
-        int digit = ogNo % 10;
-        if(digit != 0 && digit % ogNo == 0){
+        int digit = n % 10;
+        if(digit != 0 && ogNo % digit == 0){
             cnt ++;
         }
+        n = n/10;
     }
     return cnt;
 }
 
 int main(){
     int n;
-    cin >> n;
-    count(n);
+    cin>>n;
+    cout << count(n);
 }
     
