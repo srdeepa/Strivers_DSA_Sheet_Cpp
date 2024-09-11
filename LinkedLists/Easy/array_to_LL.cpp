01ac94679792c575d0e82c82687d88ec207e5208
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-12 01:12:09 +0530

    Create array_to_LL.cpp

---
+++ LinkedLists/Easy/array_to_LL.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        Node* head= new Node(arr[0]);
+        Node* mover = head;
+        for(int i =0;i<arr.size();i++){
+            Node* temp = new Node(arr[i]);
+            mover->next = temp;
+            mover = temp;
+        }
+        return head->next;
+    }
+};
