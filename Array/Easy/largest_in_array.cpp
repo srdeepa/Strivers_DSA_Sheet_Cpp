a660591f4786b4fa3baca08ebb95eac65495861c
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-11 16:48:18 +0530

    Create largest_in_array.cpp

---
+++ Array/Easy/largest_in_array.cpp
@@ -0,0 +1,21 @@
+//User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int print2largest(vector<int> &arr) {
+        // Code Here
+        int lar = arr[0]; 
+        int secLar = -1;
+
+    for(int i = 0; i < arr.size(); i++) {
+        if (arr[i] > lar) {
+            secLar = lar;
+            lar = arr[i];
+        } else if (arr[i] > secLar && arr[i] != lar) {
+            secLar = arr[i];
+        }
+    }
+    return secLar;
+    }
+};
