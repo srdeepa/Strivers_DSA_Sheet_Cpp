79cb4ad42c363c014f6cb804eeddd3e8bd7bf6ab
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-16 00:38:34 +0530

    Create Warshall's Algo(Transitive Closure).cpp

---
+++ Dynamic Programming/Warshall's Algo(Transitive Closure).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int graph[40][40], n, count = 0;
+
+void createGraph() {
+    cout << "No. of vertices>> ";
+    cin >> n;
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> graph[i][j];
+        }
+    }
+}
+
+int main() {
+    createGraph();
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                count++;
+                graph[i][j] = (graph[i][j] | (graph[i][k] & graph[k][j]));
+            }
+        }
+    }
+
+    cout << "Applying Warshall's Algorithm\n";
+    cout << "Transitive Closure Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Operation Count: " << count << endl;
+
+    return 0;
+}
