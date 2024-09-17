20df5e95a51b3a16e4089f973d92e73a14f8db64
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-17 23:30:44 +0530

    Update Minimum Element in BST.cpp

--- Binary Search Tree/Minimum Element in BST.cpp
+++ Binary Search Tree/Minimum Element in BST.cpp
@@ -1,31 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+// User function Template for C++
+
+// Function to find the minimum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
 class Solution {
-public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        TreeNode* current = root;
-        
-        // Iteratively search for the value in the BST
-        while (current != nullptr) {
-            if (val == current->val) {
-                return current;  // Found the node
-            } else if (val < current->val) {
-                current = current->left;  // Search in the left subtree
-            } else {
-                current = current->right;  // Search in the right subtree
-            }
-        }
-        
-        // If the value is not found
-        return nullptr;
+  public:
+    int maxValue(Node* root) {
+        // Code here
+        if (root == nullptr) {
+        throw runtime_error("The tree is empty.");
+    }
+
+    Node* current = root;
+    
+    // Traverse the left subtree to find the minimum value
+    while (current->left != nullptr) {
+        current = current->left;
+    }
+    
+    // Return the value of the leftmost node
+    return current->data;
     }
 };
