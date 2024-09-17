d4ac9f0b621f7d5209cf2ad8c7fb484207088a79
Deepa S R <149315130+srdeepa@users.noreply.github.com>
2024-09-17 23:29:42 +0530

    Create Maximum Element in BST.cpp

---
+++ Binary Search Tree/Maximum Element in BST.cpp
@@ -0,0 +1,42 @@
+
+// User function Template for C++
+
+// Function to find the maximum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
+class Solution {
+  public:
+    int maxValue(Node* root) {
+        // Code here
+        if (root == nullptr) {
+        throw runtime_error("The tree is empty.");
+    }
+
+    Node* current = root;
+    
+    // Traverse the left subtree to find the minimum value
+    while (current->right != nullptr) {
+        current = current->right;
+    }
+    
+    // Return the value of the leftmost node
+    return current->data;
+    }
+};
+
+
